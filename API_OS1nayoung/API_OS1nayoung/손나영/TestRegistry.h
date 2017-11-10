f027ad7c07138c6e0aa04d7cd5c998885c58d318
nayoungson <niiaig6003@nate.com>
2017-11-10 16:01:54 +0900

    손나영

--- API_OS1nayoung/API_OS1nayoung/손나영/TestRegistry.h
+++ API_OS1nayoung/API_OS1nayoung/손나영/TestRegistry.h
@@ -5,6 +5,8 @@
 #include "../OSQA.h"
 #include "../MyUtil.h"
 #include <TlHelp32.h>
+#include <stdio.h>
+#include <tchar.h>
 
 BOOL test_RegCloseKey();
 BOOL test_RegCreateKeyExW();
@@ -22,4 +24,34 @@ BOOL test_RegOpenKeyW();
 BOOL test_RegDeleteKeyW();
 BOOL test_RegNotifyChangeKeyValue();
 
+//PPT
+
+
+BOOL test_RegLoadKeyA();
+BOOL test_RegLoadKeyW();
+
+BOOL test_RegUnLoadKeyA();
+BOOL test_RegUnLoadKeyW();
+
+BOOL test_RegSaveKeyExA();
+BOOL test_RegSaveKeyExW();
+
+BOOL test_RegRestoreKeyA();
+BOOL test_RegRestoreKeyW();
+
+BOOL test_RegLoadMUIStringA();
+BOOL test_RegLoadMUIStringW();
+
+BOOL test_RegDeleteTreeW();
+BOOL test_RegEnumKeyExA();
+BOOL test_RegGetValueW();
+BOOL test_RegDisablePredefinedCacheEx();
+BOOL test_RegOpenCurrentUser();
+BOOL test_RegOpenUserClassesRoot();
+
+
+
+//ÇÑ±Û
+
+BOOL test_RegEnumValueA();
 #endif
